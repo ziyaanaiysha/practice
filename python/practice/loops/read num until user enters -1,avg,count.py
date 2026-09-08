be87f94378a617de07827a1658176ddf181aa158
Ziyaan Aiysha <ziyaanzonia@gmail.com>
2026-09-08 12:01:23 +0530

    Implement number input loop with average and count
    Added functionality to read numbers until -1 is entered, calculate average and count.

--- python/practice/loops/read num until user enters -1,avg,count.py
+++ python/practice/loops/read num until user enters -1,avg,count.py
@@ -1,1 +1,14 @@
-
+sum=0
+count=0
+while True:
+    num=int(input("Enter number: "))
+    if num==-1:
+        break
+    sum+=num
+    count+=1
+if count>0:
+    avg=sum/count
+    print("avg= ",avg)
+    print("count= ",count)
+else:
+    print('no numbers were entered')
